348ea5b4f67183e5f73d6084a9789d0c072c82b6
Mason Chinkin <mason.chinkin@gmail.com>
2021-03-06 21:34:39 -0800

    Finish map project

--- maps/src/Company.ts
+++ maps/src/Company.ts
@@ -1,15 +1,17 @@
 import faker from 'faker'
 import { Mappable } from './CustomMap'
 
-export class User implements Mappable {
-  name: string
+export class Company implements Mappable {
+  companyName: string
+  catchPhrase: string
   location: {
     lat: number
     lng: number
   }
 
   constructor() {
-    this.name = `${faker.name.firstName()} ${faker.name.lastName()}`
+    this.companyName = faker.company.companyName()
+    this.catchPhrase = faker.company.catchPhrase()
     this.location = {
       lat: parseFloat(faker.address.latitude()),
       lng: parseFloat(faker.address.longitude()),
@@ -19,7 +21,8 @@ export class User implements Mappable {
   markerContent() {
     return `
       <div>
-        <h2>User: ${this.name}</h2>
+        <h2>Company: ${this.companyName}</h2>
+        <h3>CatchPhrase: ${this.catchPhrase}</h3>
       </div>
     `
   }
